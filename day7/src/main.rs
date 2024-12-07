7cb231baf740f5534dacd2c841e60cf6b371552c
Andrew Sas <andrewfsas@gmail.com>
2024-12-07 01:59:57 -0500

    part, not day

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,10 +1,10 @@
-mod day_one;
-mod day_two;
+mod part_one;
+mod part_two;
 
 use parse_file::parse_file;
 
 fn main() {
     let input = parse_file("input");
-    println!("{}", day_one::solve_part_one(&input));
-    println!("{}", day_two::solve_part_two(&input));
+    println!("{}", part_one::solve_part_one(&input));
+    println!("{}", part_two::solve_part_two(&input));
 }
