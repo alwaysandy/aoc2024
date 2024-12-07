02e93410f15dfa5ed249fc0aafc2e181ac9d3fd9
Andrew Sas <andrewfsas@gmail.com>
2024-12-07 01:57:44 -0500

    cargo fmt

--- day7/src/day_one.rs
+++ day7/src/day_one.rs
@@ -1,7 +1,14 @@
 pub fn solve_part_one(input: &[String]) -> usize {
     input.iter().fold(0, |acc, line| {
         let final_val = line.split(":").next().unwrap().parse::<usize>().unwrap();
-        let operands: Vec<usize> = line.split(":").last().unwrap().trim().split(" ").map(|s| s.parse::<usize>().unwrap()).collect();
+        let operands: Vec<usize> = line
+            .split(":")
+            .last()
+            .unwrap()
+            .trim()
+            .split(" ")
+            .map(|s| s.parse::<usize>().unwrap())
+            .collect();
         let valid = is_valid(&operands, 0, operands[0], final_val);
         if valid {
             acc + final_val
@@ -11,7 +18,12 @@ pub fn solve_part_one(input: &[String]) -> usize {
     })
 }
 
-fn is_valid(operands: &Vec<usize>, current_index: usize, current_val: usize, final_val: usize) -> bool {
+fn is_valid(
+    operands: &Vec<usize>,
+    current_index: usize,
+    current_val: usize,
+    final_val: usize,
+) -> bool {
     if current_val == final_val && current_index == operands.len() - 1 {
         return true;
     }
@@ -20,9 +32,19 @@ fn is_valid(operands: &Vec<usize>, current_index: usize, current_val: usize, fin
         return false;
     }
 
-    if is_valid(operands, current_index + 1, current_val + operands[current_index + 1], final_val) {
+    if is_valid(
+        operands,
+        current_index + 1,
+        current_val + operands[current_index + 1],
+        final_val,
+    ) {
         return true;
     }
 
-    is_valid(operands, current_index + 1, current_val * operands[current_index + 1], final_val)
+    is_valid(
+        operands,
+        current_index + 1,
+        current_val * operands[current_index + 1],
+        final_val,
+    )
 }
