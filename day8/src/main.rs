cb5242cd6318a87e7b9f235e478220dd18cc1fa3
Andrew Sas <andrewfsas@gmail.com>
2024-12-08 20:03:30 -0500

    Day 8 part one

---
+++ day8/src/main.rs
@@ -0,0 +1,8 @@
+use parse_file::parse_file;
+
+mod part_one;
+
+fn main() {
+    let input = parse_file("input");
+    println!("{}", part_one::solve_part_one(&input));
+}
---
+++ day8/src/part_one.rs
@@ -0,0 +1,121 @@
+use itertools::structs::Combinations;
+use itertools::Itertools;
+use std::collections::{HashMap, HashSet};
+
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Left,
+    Down,
+    Right,
+    UpRight,
+    UpLeft,
+    DownRight,
+    DownLeft,
+}
+
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct XY {
+    x: i32,
+    y: i32,
+}
+
+pub fn solve_part_one(input: &[String]) -> usize {
+    let char_array: Vec<Vec<char>> = input.iter().map(|line| line.chars().collect()).collect();
+    let antennas: HashMap<char, Vec<XY>> =
+        char_array
+            .iter()
+            .enumerate()
+            .fold(HashMap::new(), |acc, (y, line)| {
+                line.iter().enumerate().fold(acc, |mut acc, (x, c)| {
+                    if *c == '.' {
+                        return acc;
+                    }
+
+                    acc.entry(*c).or_insert(vec![]).push(XY {
+                        x: x as i32,
+                        y: y as i32,
+                    });
+                    acc
+                })
+            });
+
+    let antinodes: HashSet<XY> = antennas.iter().fold(HashSet::new(), |mut acc, (c, line)| {
+        println!("{:?}", line);
+        line.iter().combinations(2).for_each(|comb| {
+            let directions = get_directions(comb[0], comb[1]);
+            let antinode_a = get_antinode(input, comb[0], comb[1], &directions.0);
+            if let Some(antinode) = antinode_a {
+                acc.insert(antinode);
+            }
+
+            let antinode_b = get_antinode(input, comb[1], comb[0], &directions.1);
+            if let Some(antinode) = antinode_b {
+                acc.insert(antinode);
+            }
+        });
+
+        acc
+    });
+
+    antinodes.len()
+}
+
+fn get_directions(a: &XY, b: &XY) -> (Direction, Direction) {
+    if a.x == b.x {
+        if a.y < b.y {
+            (Direction::Up, Direction::Down)
+        } else {
+            (Direction::Down, Direction::Up)
+        }
+    } else if a.y == b.y {
+        if a.x < b.x {
+            (Direction::Left, Direction::Right)
+        } else {
+            (Direction::Right, Direction::Left)
+        }
+    } else if a.x < b.x {
+        if a.y < b.y {
+            (Direction::UpLeft, Direction::DownRight)
+        } else {
+            (Direction::DownLeft, Direction::UpRight)
+        }
+    } else {
+        if a.y < b.y {
+            (Direction::UpRight, Direction::DownLeft)
+        } else {
+            (Direction::DownRight, Direction::UpLeft)
+        }
+    }
+}
+
+fn get_change(direction: &Direction) -> [i32; 2] {
+    match direction {
+        Direction::Up => [0, -1],
+        Direction::Down => [0, 1],
+        Direction::Left => [-1, 0],
+        Direction::Right => [1, 0],
+        Direction::UpLeft => [-1, -1],
+        Direction::DownLeft => [-1, 1],
+        Direction::UpRight => [1, -1],
+        Direction::DownRight => [1, 1],
+    }
+}
+
+fn get_antinode(input: &[String], a: &XY, b: &XY, direction: &Direction) -> Option<XY> {
+    let x_difference = (a.x - b.x).abs();
+    let y_difference = (a.y - b.y).abs();
+    let change = get_change(direction);
+    let x_change = change[0] * x_difference;
+    let y_change = change[1] * y_difference;
+    if a.x + x_change < 0 || a.x + x_difference >= input[0].len() as i32 {
+        None
+    } else if a.y + y_change < 0 || a.y + y_difference >= input.len() as i32 {
+        None
+    } else {
+        Some(XY {
+            x: a.x + x_change,
+            y: a.y + y_change,
+        })
+    }
+}
