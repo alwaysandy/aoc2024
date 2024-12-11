9bfef8e035537ea43272360e9f6de8475c323bb8
Andrew Sas <andrewfsas@gmail.com>
2024-12-11 02:12:10 -0500

    Rename variables

--- day11/src/part_two.rs
+++ day11/src/part_two.rs
@@ -1,39 +1,39 @@
 use std::collections::HashMap;
 
 pub fn solve_part_two(input: &[usize]) -> usize {
-    let mut memoizer: HashMap<(usize, usize), usize> = HashMap::new();
+    let mut cache: HashMap<(usize, usize), usize> = HashMap::new();
     input.iter().fold(0, |acc ,n| {
-        acc + blink(*n, 250, &mut memoizer)
+        acc + blink(*n, 75, &mut cache)
     })
 }
 
-fn blink(input: usize, n: usize, memoizer: &mut HashMap<(usize, usize), usize>) -> usize {
+fn blink(input: usize, n: usize, cache: &mut HashMap<(usize, usize), usize>) -> usize {
     if n == 0 {
         return 1;
     }
 
     if input == 0 {
-        blink(1, n-1, memoizer)
+        blink(1, n-1, cache)
     } else {
-        if memoizer.contains_key(&(input, n)) {
-            return memoizer[&(input, n)];
+        if let Some(v) = cache.get(&(input, n)) {
+            return *v;
         }
 
-        let power: u32 = input.ilog10() + 1;
-        if power == 0  || power % 2 != 0 {
-            let power_return = blink(input * 2024, n - 1, memoizer);
-            memoizer.insert((input, n), power_return);
+        let input_len: u32 = input.ilog10() + 1;
+        if input_len == 0  || input_len % 2 != 0 {
+            let power_return = blink(input * 2024, n - 1, cache);
+            cache.insert((input, n), power_return);
             power_return
         } else {
-            let split_return = split_digit(&input, power / 2, n, memoizer);
-            memoizer.insert((input, n), split_return);
+            let split_return = split_digit(&input, input_len / 2, n, cache);
+            cache.insert((input, n), split_return);
             split_return
         }
     }
 }
 
-fn split_digit(input: &usize, power: u32, n: usize, memoizer: &mut HashMap<(usize, usize), usize>) -> usize {
-    let input_div = blink(input / 10_i32.pow(power) as usize, n - 1, memoizer);
-    let input_mod = blink(input % 10_i32.pow(power) as usize, n - 1, memoizer);
+fn split_digit(input: &usize, power: u32, n: usize, cache: &mut HashMap<(usize, usize), usize>) -> usize {
+    let input_div = blink(input / 10_i32.pow(power) as usize, n - 1, cache);
+    let input_mod = blink(input % 10_i32.pow(power) as usize, n - 1, cache);
     input_div + input_mod
 }
