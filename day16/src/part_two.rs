a13e4981bb1aceabf928b6433993a2420f8b3329
Andrew Sas <andrewfsas@gmail.com>
2024-12-16 16:23:21 -0500

    Cleanup code + cargo fmt

--- day16/src/part_two.rs
+++ day16/src/part_two.rs
@@ -44,71 +44,99 @@ pub fn solve(input: &[Vec<char>]) {
             if scores.get(&curr).unwrap() >= &score {
                 scores.insert(curr.clone(), score);
             } else {
-                // THIS MAY NOT BE THE WAY, WE MAY NEED TO TRACK DIRECTION IN THE FUTURE????
-                // THIS IS THE PLACE THINGS WILL DEFINITELY GO WRONG
                 continue;
             }
         } else {
-            scores.insert(curr.clone(), usize::max_value());
+            scores.insert(curr.clone(), score);
         }
 
-        directions
-            .iter()
-            .for_each(|direction| {
-                let new_score = if direction == &dir {
-                    score + 1
-                } else if direction == &get_opposite_direction(&dir) {
-                    score + 2001
-                } else {
-                    score + 1001
-                };
+        directions.iter().for_each(|direction| {
+            let new_score = if direction == &dir {
+                score + 1
+            } else if direction == &get_opposite_direction(&dir) {
+                score + 2001
+            } else {
+                score + 1001
+            };
 
-                let change = get_change(direction);
-                if curr.x + change[0] < 0 || curr.y + change[1] < 0 {
+            let change = get_change(direction);
+            if curr.x + change[0] < 0
+                || curr.y + change[1] < 0
+                || curr.x + change[0] >= input[0].len() as i32
+                || curr.y + change[1] >= input.len() as i32
+            {
+                return;
+            }
+
+            let new_loc = XY {
+                x: curr.x + change[0],
+                y: curr.y + change[1],
+            };
+
+            if scores.contains_key(&new_loc) {
+                if scores.get(&new_loc).unwrap() <= &new_score {
                     return;
-                } else if curr.x + change[0] >= input[0].len() as i32 || curr.y + change[1] >= input.len() as i32 {
-                    return;
                 }
+            }
 
-                let new_loc = XY {
-                    x: curr.x + change[0],
-                    y: curr.y + change[1],
-                };
-
-                if scores.contains_key(&new_loc) {
-                    if scores.get(&new_loc).unwrap() <= &new_score {
-                        return
-                    }
-                }
-
-                if input[new_loc.y as usize][new_loc.x as usize] == '.' || input[new_loc.y as usize][new_loc.x as usize] == 'E' {
-                    queue.push_front((new_loc, direction.clone(), new_score));
-                }
-            });
+            if input[new_loc.y as usize][new_loc.x as usize] == '.'
+                || input[new_loc.y as usize][new_loc.x as usize] == 'E'
+            {
+                queue.push_front((new_loc, direction.clone(), new_score));
+            }
+        });
     }
 
-    let max_score=  scores.get(&end).unwrap();
-    let valid_squares: HashSet<XY> = scores.iter().filter_map(|(c, score)| if score <= &max_score {Some(c.clone())} else {None}).collect();
+    let max_score = scores.get(&end).unwrap();
+    let valid_squares: HashSet<XY> = scores
+        .iter()
+        .filter_map(|(c, score)| {
+            if score <= max_score {
+                Some(c.clone())
+            } else {
+                None
+            }
+        })
+        .collect();
 
-    let mut correct_path: HashSet<XY> = HashSet::new();
-    correct_path.insert(start.clone());
     let mut failed_at: HashMap<(XY, Direction), usize> = HashMap::new();
-    dfs(&valid_squares, &mut HashSet::new(), &Direction::Right, &start, &end, &mut correct_path, 0, max_score.clone(), &mut failed_at);
+    if let Some(path) = dfs(
+        &valid_squares,
+        &mut HashSet::new(),
+        &Direction::Right,
+        &start,
+        &end,
+        &mut HashSet::new(),
+        0,
+        *max_score,
+        &mut failed_at,
+    ) {
+        println!("{:?}", path.len());
+    }
 
-    println!("{:?}", correct_path.len());
-    print_all(input, &correct_path);
+    // print_all(input, &correct_path);
 }
 
-fn dfs(input: &HashSet<XY>, path: &mut HashSet<XY>, dir: &Direction, loc: &XY, end: &XY, correct_path: &mut HashSet<XY>, score: usize, max_score: usize, failed_at: &mut HashMap<(XY, Direction), usize>) -> bool {
+fn dfs(
+    input: &HashSet<XY>,
+    path: &mut HashSet<XY>,
+    dir: &Direction,
+    loc: &XY,
+    end: &XY,
+    correct_path: &mut HashSet<XY>,
+    score: usize,
+    max_score: usize,
+    failed_at: &mut HashMap<(XY, Direction), usize>,
+) -> Option<HashSet<XY>> {
     if failed_at.contains_key(&(loc.clone(), dir.clone())) {
         if &score >= failed_at.get(&(loc.clone(), dir.clone())).unwrap() {
-            return false;
+            return None;
         }
     }
 
     if loc == end {
         correct_path.insert(loc.clone());
-        return true;
+        return Some(correct_path.clone());
     }
 
     let directions = get_all_directions();
@@ -118,7 +146,7 @@ fn dfs(input: &HashSet<XY>, path: &mut HashSet<XY>, dir: &Direction, loc: &XY, e
     directions.iter().for_each(|next_dir| {
         let new_score = if dir == next_dir {
             score + 1
-        } else if &get_opposite_direction(&dir) == next_dir {
+        } else if &get_opposite_direction(dir) == next_dir {
             score + 2001
         } else {
             score + 1001
@@ -142,25 +170,40 @@ fn dfs(input: &HashSet<XY>, path: &mut HashSet<XY>, dir: &Direction, loc: &XY, e
             return;
         }
 
-        if dfs(input, path, next_dir, &next_loc, end, correct_path, new_score, max_score, failed_at) {
+        if let Some(added_paths) = dfs(
+            input,
+            path,
+            next_dir,
+            &next_loc,
+            end,
+            correct_path,
+            new_score,
+            max_score,
+            failed_at,
+        ) {
             is_valid = true;
-            correct_path.insert(next_loc.clone());
+            correct_path.extend(added_paths);
+            correct_path.insert(loc.clone());
         }
     });
 
-    path.remove(&loc);
+    path.remove(loc);
 
     if !is_valid {
         failed_at.insert((loc.clone(), dir.clone()), score);
+        None
+    } else {
+        Some(correct_path.clone())
     }
-    is_valid
 }
 
 fn print_all(input: &[Vec<char>], valid_squares: &HashSet<XY>) {
-    let mut editable = input.clone().to_owned();
+    let mut editable = input.to_owned();
     valid_squares.iter().for_each(|s| {
         editable[s.y as usize][s.x as usize] = '$';
     });
 
-    editable.iter().for_each(|s| {println!("{}", s.into_iter().collect::<String>())})
+    editable
+        .iter()
+        .for_each(|s| println!("{}", s.iter().collect::<String>()))
 }
