b84b85ddb4eca858f4d7ea707f693fd53fcd28cf
Andrew Sas <andrewfsas@gmail.com>
2024-12-19 04:04:49 -0500

    Code cleanup

--- day19/src/part_two.rs
+++ day19/src/part_two.rs
@@ -1,9 +1,7 @@
 use std::collections::{HashMap, HashSet};
 
 pub fn solve(input: &[String]) {
-    let patterns: HashSet<String> = input[0].split(", ").map(|p| {
-        p.to_string()
-    }).collect();
+    let patterns: HashSet<String> = input[0].split(", ").map(|p| p.to_string()).collect();
 
     let answer = input[2..].iter().fold(0, |acc, s| {
         let mut succeeded: HashMap<(usize, String), usize> = HashMap::new();
@@ -13,7 +11,11 @@ pub fn solve(input: &[String]) {
     println!("{}", answer);
 }
 
-fn count_possibilities(towel: &str, patterns: &HashSet<String>, succeeded: &mut HashMap<(usize, String), usize>) -> usize {
+fn count_possibilities(
+    towel: &str,
+    patterns: &HashSet<String>,
+    succeeded: &mut HashMap<(usize, String), usize>,
+) -> usize {
     if towel.len() == 0 {
         return 1;
     }
@@ -27,13 +29,13 @@ fn count_possibilities(towel: &str, patterns: &HashSet<String>, succeeded: &mut 
 
         if patterns.contains(&towel[0..i]) {
             let tobenamed = &towel[i..];
-            if succeeded.contains_key(&(i, tobenamed.to_string())) {
-                count += succeeded.get(&(i, tobenamed.to_string())).unwrap();
+            if let Some(answer) = succeeded.get(&(i, tobenamed.to_string())) {
+                count += answer;
                 i -= 1;
                 continue;
             }
 
-            let answer= count_possibilities(&tobenamed, patterns, succeeded);
+            let answer = count_possibilities(&tobenamed, patterns, succeeded);
             if answer == 0 {
                 i -= 1;
                 continue;
